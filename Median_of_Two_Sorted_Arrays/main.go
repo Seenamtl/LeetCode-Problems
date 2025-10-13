fc845d16f64986a3a0ba3cc701565ca81835ba5c
Sina Ghalebi <56813104+Seenamtl@users.noreply.github.com>
2025-10-13 15:17:41 +0330

    Update main.go

--- Median_of_Two_Sorted_Arrays/main.go
+++ Median_of_Two_Sorted_Arrays/main.go
@@ -60,9 +60,9 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 }
 
 func main() {
-	fmt.Println(findMedianSortedArrays([]int{1, 3}, []int{2}))    // 2
-	fmt.Println(findMedianSortedArrays([]int{1, 2}, []int{3, 4})) // 2.5
-	fmt.Println(findMedianSortedArrays([]int{0, 0}, []int{0, 0})) // 0
-	fmt.Println(findMedianSortedArrays([]int{}, []int{1}))        // 1
-	fmt.Println(findMedianSortedArrays([]int{2}, []int{}))        // 2
+	fmt.Println(findMedianSortedArrays([]int{1, 3}, []int{2}))
+	fmt.Println(findMedianSortedArrays([]int{1, 2}, []int{3, 4}))
+	fmt.Println(findMedianSortedArrays([]int{0, 0}, []int{0, 0}))
+	fmt.Println(findMedianSortedArrays([]int{}, []int{1}))
+	fmt.Println(findMedianSortedArrays([]int{2}, []int{}))
 }
